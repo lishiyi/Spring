7c90d181b2b7a62b090784dd58202f94de939a2c
lishiyi <lishiyiiii@gmail.com>
2016-06-19 19:16:24 -0400

    Bean Inheritance and Template

--- HelloSpring/src/info/shiyi/MainApp.java
+++ HelloSpring/src/info/shiyi/MainApp.java
@@ -1,13 +1,20 @@
 package info.shiyi;
 
-import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MainApp {
-	   public static void main(String[] args) {
-	      AbstractApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
-	      HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
-	      obj.getMessage();
-	      context.registerShutdownHook();
-	   }
-	}
+   public static void main(String[] args) {
+      ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+
+      HelloWorld objA = (HelloWorld) context.getBean("helloWorld");
+
+      objA.getMessage1();
+      objA.getMessage2();
+
+      HelloIndia objB = (HelloIndia) context.getBean("helloIndia");
+      objB.getMessage1();
+      objB.getMessage2();
+      objB.getMessage3();
+   }
+}
