db443a2dd6e62e0fcb2344376a9c38ca58cbcee0
lishiyi <lishiyiiii@gmail.com>
2016-06-19 18:32:32 -0400

    Bean Init and Destroy

--- HelloSpring/src/info/shiyi/HelloWorld.java
+++ HelloSpring/src/info/shiyi/HelloWorld.java
@@ -1,11 +1,19 @@
 package info.shiyi;
 
+
 public class HelloWorld {
-	   private String message;
-	   public void setMessage(String message){
-	      this.message  = message;
-	   }
-	   public void getMessage(){
-	      System.out.println("Your Message : " + message);
-	   }
-	}
+   private String message;
+
+   public void setMessage(String message){
+      this.message  = message;
+   }
+   public void getMessage(){
+      System.out.println("Your Message : " + message);
+   }
+   public void init(){
+      System.out.println("Bean is going through init.");
+   }
+   public void destroy(){
+      System.out.println("Bean will destroy now.");
+   }
+}
--- HelloSpring/src/info/shiyi/MainApp.java
+++ HelloSpring/src/info/shiyi/MainApp.java
@@ -1,14 +1,13 @@
 package info.shiyi;
-import org.springframework.context.ApplicationContext;
+
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MainApp {
-   public static void main(String[] args) {
-      ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
-      HelloWorld objA = (HelloWorld) context.getBean("helloWorld");
-      objA.setMessage("I'm object A");
-      objA.getMessage();
-      HelloWorld objB = (HelloWorld) context.getBean("helloWorld");
-      objB.getMessage();
-   }
-}
+	   public static void main(String[] args) {
+	      AbstractApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+	      HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
+	      obj.getMessage();
+	      context.registerShutdownHook();
+	   }
+	}
