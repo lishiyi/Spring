5d45b7604f859c2260e03099ae13955ca816f2cc
lishiyi <lishiyiiii@gmail.com>
2016-06-19 18:24:59 -0400

    Scope of Beans
    Differences between "singleton" and "prototype"

--- HelloSpring/src/info/shiyi/MainApp.java
+++ HelloSpring/src/info/shiyi/MainApp.java
@@ -4,9 +4,11 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MainApp {
    public static void main(String[] args) {
-      ApplicationContext context = 
-             new ClassPathXmlApplicationContext("Beans.xml");
-      HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
-      obj.getMessage();
+      ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+      HelloWorld objA = (HelloWorld) context.getBean("helloWorld");
+      objA.setMessage("I'm object A");
+      objA.getMessage();
+      HelloWorld objB = (HelloWorld) context.getBean("helloWorld");
+      objB.getMessage();
    }
 }
