a1fe1387508d4fec023d40afc6257ecbef2e2c20
lishiyi <lishiyiiii@gmail.com>
2016-06-21 14:52:08 -0400

    Annotation - JSR-250

--- HelloSpring/src/info/shiyi/HelloWorld.java
+++ HelloSpring/src/info/shiyi/HelloWorld.java
@@ -1,18 +1,21 @@
 package info.shiyi;
 
+import javax.annotation.*;
 public class HelloWorld {
-	   private String message1;
-	   private String message2;
-	   public void setMessage1(String message){
-	      this.message1  = message;
-	   }
-	   public void setMessage2(String message){
-	      this.message2  = message;
-	   }
-	   public void getMessage1(){
-	      System.out.println("World Message1 : " + message1);
-	   }
-	   public void getMessage2(){
-	      System.out.println("World Message2 : " + message2);
-	   }
-	}
+   private String message;
+   public void setMessage(String message){
+      this.message  = message;
+   }
+   public String getMessage(){
+      System.out.println("Your Message : " + message);
+      return message;
+   }
+   @PostConstruct
+   public void init(){
+      System.out.println("Bean is going through init.");
+   }
+   @PreDestroy
+   public void destroy(){
+      System.out.println("Bean will destroy now.");
+   }
+}
--- HelloSpring/src/info/shiyi/MainApp.java
+++ HelloSpring/src/info/shiyi/MainApp.java
@@ -1,12 +1,13 @@
 package info.shiyi;
 
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class MainApp {
    public static void main(String[] args) {
-      ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
-      Profile profile = (Profile) context.getBean("profile");
-      profile.printAge();
-      profile.printName();
+      AbstractApplicationContext context = 
+                          new ClassPathXmlApplicationContext("Beans.xml");
+      HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
+      obj.getMessage();
+      context.registerShutdownHook();
    }
 }
