db1a6d3a30181b5f98752fde6470a8a5271199d0
lishiyi <lishiyiiii@gmail.com>
2016-06-20 21:53:19 -0400

    Annotation - Required

--- HelloSpring/src/info/shiyi/MainApp.java
+++ HelloSpring/src/info/shiyi/MainApp.java
@@ -4,9 +4,9 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class MainApp {
    public static void main(String[] args) {
-      ApplicationContext context = 
-             new ClassPathXmlApplicationContext("Beans.xml");
-      TextEditor te = (TextEditor) context.getBean("textEditor");
-      te.spellCheck();
+      ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+      Student student = (Student) context.getBean("student");
+      System.out.println("Name : " + student.getName() );
+      System.out.println("Age : " + student.getAge() );
    }
 }
