729d67e28a0066a660e39cb2e4849a53db302bde
lishiyi <lishiyiiii@gmail.com>
2016-06-21 19:27:08 -0400

    AOP - @AspectJ

--- HelloSpring/src/info/shiyi/MainApp.java
+++ HelloSpring/src/info/shiyi/MainApp.java
@@ -1,14 +1,14 @@
 package info.shiyi;
 
-import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class MainApp {
    public static void main(String[] args) {
-      ConfigurableApplicationContext context = 
-      new ClassPathXmlApplicationContext("Beans.xml");    
-      CustomEventPublisher cvp = 
-      (CustomEventPublisher) context.getBean("customEventPublisher");
-      cvp.publish();  
-      cvp.publish();
+      ApplicationContext context = 
+             new ClassPathXmlApplicationContext("Beans.xml");
+      Student student = (Student) context.getBean("student");
+      student.getName();
+      student.getAge();     
+      student.printThrowException();
    }
 }
--- HelloSpring/src/info/shiyi/Student.java
+++ HelloSpring/src/info/shiyi/Student.java
@@ -5,14 +5,20 @@ public class Student {
 	   private String name;
 	   public void setAge(Integer age) {
 	      this.age = age;
-	   }   
+	   }
 	   public Integer getAge() {
+	      System.out.println("Age : " + age );
 	      return age;
 	   }
 	   public void setName(String name) {
 	      this.name = name;
-	   }  
+	   }
 	   public String getName() {
+	      System.out.println("Name : " + name );
 	      return name;
+	   }
+	   public void printThrowException(){
+	      System.out.println("Exception raised");
+	      throw new IllegalArgumentException();
 	   }
 	}
