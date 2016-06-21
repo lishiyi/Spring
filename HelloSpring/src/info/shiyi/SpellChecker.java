0f1ff3cf0ee7f5a75775de569b0af495f9019b4e
lishiyi <lishiyiiii@gmail.com>
2016-06-20 21:05:56 -0400

    Inner Beans

--- HelloSpring/src/info/shiyi/SpellChecker.java
+++ HelloSpring/src/info/shiyi/SpellChecker.java
@@ -4,7 +4,7 @@ public class SpellChecker {
 	   public SpellChecker(){
 	      System.out.println("Inside SpellChecker constructor." );
 	   }
-	   public void checkSpelling() {
+	   public void checkSpelling(){
 	      System.out.println("Inside checkSpelling." );
-	   } 
+	   }   
 	}
--- HelloSpring/src/info/shiyi/TextEditor.java
+++ HelloSpring/src/info/shiyi/TextEditor.java
@@ -2,9 +2,14 @@ package info.shiyi;
 
 public class TextEditor {
 	   private SpellChecker spellChecker;
-	   public TextEditor(SpellChecker spellChecker) {
-	      System.out.println("Inside TextEditor constructor." );
+	   // a setter method to inject the dependency.
+	   public void setSpellChecker(SpellChecker spellChecker) {
+	      System.out.println("Inside setSpellChecker." );
 	      this.spellChecker = spellChecker;
+	   }  
+	   // a getter method to return spellChecker
+	   public SpellChecker getSpellChecker() {
+	      return spellChecker;
 	   }
 	   public void spellCheck() {
 	      spellChecker.checkSpelling();
