d5462d8bb35b74aa8e44a2e620febaf1f4f7482c
lishiyi <lishiyiiii@gmail.com>
2016-07-01 18:49:06 -0400

    JDBC_procedure

--- HelloSpring/src/info/shiyi/MainApp.java
+++ HelloSpring/src/info/shiyi/MainApp.java
@@ -4,13 +4,12 @@ import java.util.List;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import info.shiyi.StudentJDBCTemplate;
-
 public class MainApp {
    public static void main(String[] args) {
       ApplicationContext context = 
              new ClassPathXmlApplicationContext("Beans.xml");
       StudentJDBCTemplate studentJDBCTemplate = 
-      (StudentJDBCTemplate)context.getBean("studentJDBCTemplate");    
+      (StudentJDBCTemplate)context.getBean("studentJDBCTemplate");     
       System.out.println("------Records Creation--------" );
       studentJDBCTemplate.create("Zara", 11);
       studentJDBCTemplate.create("Nuha", 2);
@@ -22,8 +21,6 @@ public class MainApp {
          System.out.print(", Name : " + record.getName() );
          System.out.println(", Age : " + record.getAge());
       }
-      System.out.println("----Updating Record with ID = 2 -----" );
-      studentJDBCTemplate.update(2, 20);
       System.out.println("----Listing Record with ID = 2 -----" );
       Student student = studentJDBCTemplate.getStudent(2);
       System.out.print("ID : " + student.getId() );
--- HelloSpring/src/info/shiyi/StudentDAO.java
+++ HelloSpring/src/info/shiyi/StudentDAO.java
@@ -24,15 +24,4 @@ public interface StudentDAO {
     * all the records from the Student table.
     */
    public List<Student> listStudents();
-   /** 
-    * This is the method to be used to delete
-    * a record from the Student table corresponding
-    * to a passed student id.
-    */
-   public void delete(Integer id);
-   /** 
-    * This is the method to be used to update
-    * a record into the Student table.
-    */
-   public void update(Integer id, Integer age);
 }
