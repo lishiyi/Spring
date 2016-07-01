4e0c802bc217362190ca7696b288e9367a4191e1
lishiyi <lishiyiiii@gmail.com>
2016-06-30 21:02:58 -0400

    JDBC

--- HelloSpring/src/info/shiyi/Student.java
+++ HelloSpring/src/info/shiyi/Student.java
@@ -3,22 +3,23 @@ package info.shiyi;
 public class Student {
 	   private Integer age;
 	   private String name;
+	   private Integer id;
 	   public void setAge(Integer age) {
 	      this.age = age;
 	   }
 	   public Integer getAge() {
-	      System.out.println("Age : " + age );
 	      return age;
 	   }
 	   public void setName(String name) {
 	      this.name = name;
 	   }
 	   public String getName() {
-	      System.out.println("Name : " + name );
 	      return name;
 	   }
-	   public void printThrowException(){
-	      System.out.println("Exception raised");
-	      throw new IllegalArgumentException();
+	   public void setId(Integer id) {
+	      this.id = id;
+	   }
+	   public Integer getId() {
+	      return id;
 	   }
 	}
